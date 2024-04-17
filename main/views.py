705aea0d314a0df713a7e703c8cd97c6d737b13d
Zrossiz <soon.pluxury@mail.ru>
2024-04-17 20:13:49 +0300

    feat: added jinja

--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def index(req):
-    return HttpResponse("<h1>Hello world</h1>")
+    return render(req, './main/index.html')
+
+
+def about(req):
+    return render(req, './main/about.html')
